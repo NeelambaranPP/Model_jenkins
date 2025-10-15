b93ecb973916378b86d2c3c037edbc32234c2080
NeelambaranPP <145982256+NeelambaranPP@users.noreply.github.com>
2025-10-15 09:28:30 +0530

    Create std.py

---
+++ std.py
@@ -0,0 +1,18 @@
+
+students = []
+
+def add_student(name, regno):
+    student = {"name": name, "regno": regno}
+    students.append(student)
+    return student
+
+def get_all_students():
+    return students
+
+if __name__ == "__main__":
+    add_student("Neel", "SSN001")
+    add_student("Arjun", "SSN002")
+
+    print("Student List:")
+    for s in get_all_students():
+        print(f"Name: {s['name']} | RegNo: {s['regno']}")
